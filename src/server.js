29c79c5e296b73794e160f6dc5b8caf1bb39542e
Luis Casas <luisscasas45@gmail.com>
2025-04-28 22:25:49 -0600

    FEAT/ update CORS origin to correct URL for deployment

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:3000", "https://admin-flow-api.vercel.app/"],
+    origin: ["http://localhost:3000", "https://admin-flow-ten.vercel.app/"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
